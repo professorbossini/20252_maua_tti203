22396d97ddaf43f2a61fd7ceb20b78fcd2aa2e97
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-21 10:59:53 -0300

    ap02-dart: encerra a apostila

--- projetos_dart/03_intro/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
+++ projetos_dart/03_intro/pedra_papel_tesoura/lib/pedra_papel_tesoura.dart
@@ -1,29 +1,52 @@
+import 'dart:io';
+import 'dart:math';
 //obrigatório usar enum e geração de valores aleatórios
+enum OPCAO {pedra, papel, tesoura, sair}
+
+void exibe(String texto){
+  print(texto);
+}
+int pegaOpcaoDoUsuario(){
+  return int.parse(stdin.readLineSync()!);
+}
+
+bool opcaoEhValida(int op){
+  return op >= 1 && op <= 4;  
+}
+//OPCAO.values: [pedra, papel, tesoura, sair]
+//usuário digita valores entre 1 e 4
+OPCAO mapeiaOpcao(int op){
+  return OPCAO.values[op - 1];
+}
+//Empate, Você venceu, Computador venceu
+String decideResultado(OPCAO opcaoUsuario, OPCAO opcaoComputador){
+  if (opcaoUsuario == opcaoComputador) return "Empate";
+  const voceVenceu = 'Você venceu';
+  const computadorVenceu = 'Computador venceu';
+  if(opcaoUsuario == OPCAO.pedra && opcaoComputador == OPCAO.tesoura) return voceVenceu;
+  if(opcaoUsuario == OPCAO.papel && opcaoComputador == OPCAO.pedra)return voceVenceu;
+  if(opcaoUsuario == OPCAO.tesoura && opcaoComputador == OPCAO.papel)return voceVenceu;
+  return computadorVenceu;  
+}
 void jogo(){
-  //exibir o menu
-
-  //capturar a opção do usuário, validando
-
-  //se o usuário digitar 4, sair
-
-  //senão
-  
-  //sortear a opção do computador
-
-  //mapear opção do usuário de int para enum
-
-  //mapear opção do computador de int para enum
-
-  //exibir as opções de cada um 
-  //pode ser assim
-  //(pedra(usuario) VS papel(computador))
-
-  //decidir quem venceu ou se houve empate
-
-  //exibir o resultado
-
-  //dormir três segundos
-
-  //continuar repetindo até o usuário digitar 4
-
+  int opcaoUsuarioInt;
+  OPCAO opcaoUsuario, opcaoComputador;
+  do{
+    do{
+      exibe('1-pedra\n2-papel\n3-tesoura\n4-sair');
+      opcaoUsuarioInt = pegaOpcaoDoUsuario();
+    }while(!opcaoEhValida(opcaoUsuarioInt));
+    opcaoUsuario = mapeiaOpcao(opcaoUsuarioInt);
+    switch(opcaoUsuario){
+      case OPCAO.sair:
+      exibe("Ok, Até");
+      default:
+      opcaoComputador = mapeiaOpcao(Random().nextInt(3) + 1);
+      //Você(pedra) VS (tesoura)Computador
+      exibe('Você(${opcaoUsuario.name}) VS (${opcaoComputador.name})Computador');
+      final resultado = decideResultado(opcaoUsuario, opcaoComputador);
+      exibe(resultado);
+      sleep(Duration(seconds: 3));
+    }
+  }while(opcaoUsuario != OPCAO.sair);
 }
