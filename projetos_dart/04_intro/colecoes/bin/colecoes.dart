3bf7d0ba1eb28964ac33cc88c1e4361bb964c4a3
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-28 12:13:05 -0300

    ap03-dart: pg 11
    - exercicio de paises com conjuntos proposto
    - retomar falando de mapas

--- projetos_dart/04_intro/colecoes/bin/colecoes.dart
+++ projetos_dart/04_intro/colecoes/bin/colecoes.dart
@@ -1,6 +1,51 @@
 import 'package:colecoes/colecoes.dart' as colecoes;
 
 void main(List<String> arguments) {
+  var portugues = {'Brasil', 'Portugal'};
+  var europa = {'Alemanha', 'Portugal', 'Espanha'};
+  // var A = {1, 2, 3, 4, 5, 6};
+  // var B = {1, 3, 7};
+  // print(A.union(B));
+  // print(A.intersection(B));
+  // print(A.difference(B));
+  // print(B.difference(A));
+  // var numeros = {15, 20};
+  // for(final n in numeros){
+  //   print(n);
+  // }
+  // print(numeros.elementAt(0));
+  // // var eAgora = {};
+  // Set <Object> seila = {"a", 1};
+  // Set numeros = {1, 1.2};
+  // var l1 = [1, 1, 2];
+  // var l2 = [1, 2, 1];
+  // var s1 = {1, 1, 2};
+  // var s2 = {1, 2, 1};
+  // var nomes = {"Ana", "João", null};
+  //sets ou conjuntos
+  // 1, 2 = 2, 1
+  // 1, 2, 2, 3 = 3, 1, 2
+  // (String, int, bool, bool) tupla = ('Ana', 18, true, true);
+  // print(tupla);
+  // print(tupla.runtimeType);
+  // print(tupla.$1);
+  // print(tupla.$2);
+  // print(tupla.$3);
+  // print(tupla.$4);
+  // var nomes1 = ['Ana', 'Pedro'];
+  // nomes1.add(null);
+  // nomes1 = null;
+  // // nomes1.add(1);
+
+  // var nomes2 = [];
+  // nomes2.add(1);
+  // nomes2.add("Ana");
+  // nomes2.add(null);
+  // // const nomes = ['Ana', 'Pedro'];
+  // nomes[0] = 'Ana Silva';
+  // nomes.add("João");
+  // print(nomes);
+  // nomes = [];
   //type annotation
   // var somenteStrings = <String> ["Ana", "João"];
   // List lista = [1];
