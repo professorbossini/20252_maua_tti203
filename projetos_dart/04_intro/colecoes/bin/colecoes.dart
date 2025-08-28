e0ebe8da673273725aab763cf20e2c1cc0c2752a
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-28 11:04:22 -0300

    ap03-dart: pg 11
    - proposta de exercicio de paises feita em aula
    - retomar falando de mapas

--- projetos_dart/04_intro/colecoes/bin/colecoes.dart
+++ projetos_dart/04_intro/colecoes/bin/colecoes.dart
@@ -1,6 +1,62 @@
 import 'package:colecoes/colecoes.dart' as colecoes;
 
 void main(List<String> arguments) {
+  //união, intersecção e diferença
+  var portugues = {'Brasil', 'Portugal'};
+  var europeus = {'Alemanha', 'Portugal', 'Espanha'};
+  // var A = {1, 2, 3, 4, 5, 6};
+  // var B = {7};
+  // print(A.union(B));
+  // print(B.union(A));
+  // print(A.intersection(B));
+  // print(B.intersection(A));
+  // print(A.difference(B));
+  // print(B.difference(A));
+  // // Set <int> numeros = {1, 2};
+  // print(numeros[0]);
+  // numeros[0] = 12;
+  // print(numeros.elementAt(0));
+  // for(final n in numeros){
+  //   print(n);
+  // }
+  // Ana: 45, Rodrigo: 70.5
+  // Map<String?, num?>? teste =  {};
+  // var nomes = <String?> {'Ana', 'João'};
+  // Set <int>?  numeros = {1, 2};
+  // //var numeros = [1, 2, 2];
+  // // numeros = null;
+  // numeros.add(2);
+  // print(numeros);
+  // 1, 2 != 2, 1
+  //1, 2 = 2, 1
+  // 1, 2, 3, 3, 3, 2 = 1, 2, 3 = 3, 3, 3, 2, 2, 2, 1, 1, 1
+  // Object tupla = ('Ana', 18, true, true);
+  // tupla = ("Rodrigo", 18, true, false);
+  // print(tupla);
+  // print(tupla.runtimeType);
+  //print(tupla.$1);
+  // print(tupla.$2);
+  // print(tupla.$3);
+  // print(tupla.$4);
+  // tupla[0] = "Rodrigo Bossini";
+  // var nomes = <String?> [null];
+  // List <Object?> nomes = ['Ana', 'Pedro', 1, bool, null];
+  // nomes.add(null);
+  // nomes.add(2.5);
+  // print(nomes[0]);
+  // print(nomes[2].length);
+  // print(nomes[3].length);
+  // print(nomes[4].falar());
+  // nomes.add(1);
+  // nomes = null;
+  // if(nomes != null){
+  //   nomes.add(null);
+  // }
+  //const nomes = ['Ana', 'Pedro'];
+  // nomes = ['João'];
+  // nomes[0] = 'Ana Silva';
+  //nomes[0] = 'Ana Silva';
+  //nomes = ['João'];
   //type annotation
   // var somenteStrings = <String> ["Ana", "João"];
   // List lista = [1];
