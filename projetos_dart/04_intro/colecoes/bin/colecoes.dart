589b6c34a2445c5d96e62937da91868d5510e657
Rodrigo Bossini <professorbossini@gmail.com>
2025-08-28 09:08:20 -0300

    ap03-dart: encerra pg 11
    - fizemos o exercicio de paises
    - retomar falando sobre mapas

--- projetos_dart/04_intro/colecoes/bin/colecoes.dart
+++ projetos_dart/04_intro/colecoes/bin/colecoes.dart
@@ -1,6 +1,58 @@
 import 'package:colecoes/colecoes.dart' as colecoes;
 
 void main(List<String> arguments) {
+  var portugues = {'Brasil', 'Portugal'};
+  var europa = {'Alemanha', 'Portugal', 'Espanha'};
+  
+  // var A = {1, 2, 3, 4, 5, 6};
+  // var B = {1, 3, 7};
+  // print(A.union(B));
+  // print(A.intersection(B));
+  // print(A.difference(B));
+  // print(B.difference(A));
+  // var nomes = {'Ana', 'Rodrigo'};
+  // // print(nomes[0]);
+  // //print(nomes.elementAt(0));
+  // for(final nome in nomes){
+  //   print(nome);
+  // }
+  //Ana:3, Rodrigo:7
+  // var numeros = <String?, int?> {};
+  //conjuntos
+  // var nomes = {'Ana', 'Ana'};
+  // print(nomes);
+  //tuplas
+  // var tupla = ('Ana', 18, true, null, 2);
+  // print(tupla);
+  // print(tupla.runtimeType);
+  // print(tupla.$1);
+  // print(tupla.$2);
+  // print(tupla.$3);
+  // print(tupla.$4);
+  // print(tupla.$5);
+
+  // List<num?>
+  //List<Object?> qual a diferença entre List<dynamic>
+  // var lista = [];
+  // lista.add(Pessoa());
+  // lista.add(1);
+  // lista[1].falar();
+  // List<String?>? nomes = [];
+  // nomes.add('Ana');
+  // nomes.add(null);
+  // nomes = null;
+  // // var nomes2 = [];
+  // nomes2.add(1);
+  // nomes2.add('Ana');
+  // nomes2.add(null);
+
+  // final nomes = ['Ana', 'Pedro'];
+  // nomes[0] = null;
+  //nomes = null
+  //nomes[x] = null
+  // var nomes = ['Ana', 'João'];
+  // nomes[0] = 'Ana Silva';
+  // nomes = ['Pedro'];
   //type annotation
   // var somenteStrings = <String> ["Ana", "João"];
   // List lista = [1];
