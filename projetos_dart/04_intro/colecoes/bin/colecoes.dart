2d3f3e8e38d4e52401d78a5c9e46bac4751f9e60
evandro <ferraz.evandro@outlook.com>
2025-08-29 15:10:58 -0300

    ap03:pag11

--- projetos_dart/04_intro/colecoes/bin/colecoes.dart
+++ projetos_dart/04_intro/colecoes/bin/colecoes.dart
@@ -1,113 +1,92 @@
-import 'package:colecoes/colecoes.dart' as colecoes;
 
 import 'dart:io';
 import 'dart:math';
 
-void jogoMegaSena(){
-  // 1- Pede para o usuário digitar seu jogo da mega
+void jogoMegaSena() {
+  //1- Pedir para o usuário digitar seu jogo
   List<int> jogoUsuario = [];
 
-  while(jogoUsuario.length < 6){
+  while (jogoUsuario.length < 6) {
+    print("Digite um dos números do seu jogo:");
     // String?
-    print("Digite um dos números do seu jogo: ");
     var num = int.parse(stdin.readLineSync()!);
 
-    // se o numero não esta entre 1 e 60
-    if(num < 1 || num > 60){
-      print("Número deve estar entre 1 e 60!!");
-    // verificando se a lista ja contem o numero
-    }else if(jogoUsuario.contains(num)){ 
-      print("Número já foi escolhido");
-    }else{
+    // se numero invalido
+    if (num < 1 || num > 60) {
+      print("Número deve estar entre 1 e 60");
+    } else if (jogoUsuario.contains(num)) {
+      // se é repetido
+      print("Número já foi digitado");
+    } else {
+      // número pode ser adicionado
       jogoUsuario.add(num);
     }
   }
 
-  //2 - Gera os números da mega
+  //2 - Gera os números da MegaSena
   Random random = Random();
   List<int> jogoSorteado = [];
 
-  while(jogoSorteado.length < 6){
-    // gerar um numero aleatorio e colocar em jogoSorteado
-    int num = random.nextInt(60) + 1; // 1 a 60
-    if(!jogoSorteado.contains(num)){ // se o numero nao estiver em jogoSorteado
-        jogoSorteado.add(num);
+  while (jogoSorteado.length < 6) {
+    // gerar um numero aleatorio
+    int num = random.nextInt(60) + 1; // 1 ate 60
+    if (!jogoSorteado.contains(num)) {
+      // se o número não é repetido
+      jogoSorteado.add(num);
     }
   }
 
-  //3- Exibir os dois jogos ordenados e lado a lado
+  //3 - Ordenar os jogos e mostrar lado a lado
   jogoSorteado.sort();
   jogoUsuario.sort();
   print("Usuario: $jogoUsuario");
   print("Sorteado: $jogoSorteado");
 
-  //4- Mostrar ao usuários quais número (e quantos) ele acertou
-  // criando um laço que compara todos os elementos de jogoUsuario com os de jogoSorteado
-  // os que respeitarem a condição jogoSorteado.contains(n) são adicionados
-  List<int> acertos = jogoUsuario.where((n) => jogoSorteado.contains(n)).toList();
-
+  // 4- Mostrar quais número e quantos o usuário acertou
+  List<int> acertos =
+      jogoUsuario.where((n) => jogoSorteado.contains(n)).toList();
   print("Acertos: $acertos");
   print("Total: ${acertos.length}");
 }
 
 void main(List<String> arguments) {
-  
-  //jogoMegaSena();
+  // jogoMegaSena();
 
-  // Testes com Coleções: Conjuntos
-  // Exemplos com Conjuntos
+  // Testes de Coleção a partir da página ap03:07
+  // 2.3 Tupla
+  var tupla = ("Ana", 18, true);
+  print(tupla); //(Ana, 18, true)
+  print(tupla.runtimeType); //(String, int, bool)
+
+  print(tupla.$1); // Ana
+  print(tupla.$2); // 18
+  print(tupla.$3); // true
+  //print(tupla.$4);
+
+  //2.4 Conjuntos: Não aceitam valores repetidos
   var nomes = {"Ana", "João"};
-  print(nomes); // Ana Joao
-  print(nomes.runtimeType); //Set<String>
+  print(nomes);
+  print(nomes.runtimeType); //_Set<String>
 
   var paises = {"Brasil", "Brasil"};
-  print(paises); // imprime só Brasil, o conjunto desconsidera valores repetidos
+  print(paises); // imprime Brasil
 
-  // criação de um mapa
-  // Map<dynamic,dynamic>
-  var mapa = {}; // estrutura chave valor, cada posicao vai ser uma combinacao chave/valor
-  print(mapa.runtimeType);
+  // Cria um Mapa
+  // Mapa = {chave:valor}
+  // Frutas = {banana:15, morango:20, laranja:40}
+  var nomes1 = {}; // Mapa <dynamic, dynamic>
+  var nomes2 = <String> {}; // Conjunto de Strings
+  var alunoPassou = <String, bool> {}; // Mapa <String, bool>; {Joao:true, Mario:false}
 
-  var paises2 = <String>{}; //Só um tipo de dado especificado = conjunto
-  print(paises2.runtimeType); // _Set<String>
-
-  var precoFrutas = <String, num>{};// Dois tipos de dados especificados = mapa
-  print(precoFrutas.runtimeType); // _Map{String, num}
-
-  // Testes com Coleções
-  //type annotation
-  // var somenteStrings = <String> ["Ana", "João"];
-  // List lista = [1];
-  // List <String> nomes = ['Ana', 'João'];
-  // List <int> numeros = [1, 2];
-  // var listas = [nomes, nomes];
-  // List<Object> l1 = ['Ana', true, 1, 1.2];
-  // List<dynamic> l2 = ['Ana', true, 1, 1.2];
-  // l1[0].falar();
-  // l2[0].falar();
+  var nomes3 = {'Ana', 'João'};
+  print(nomes3.elementAt(0)); // Ana
   
-  // print(nomes.first.toString());
-  // print(nomes.firstOrNull);
-  // var ac = 0;
-  // for(var i = 0; i < arguments.length; i++){
-  //   ac = ac + int.parse(arguments[i]);
-  // }
-  // print(ac);
-  // var numeros = [1, 2.5];
-  // var itensDiversos = ['Ana', true, 2, 2.5];
-  // var nomes = ['João', 'Pedro', 'Maria'];
-  // nomes[0] = 2;
-  //for each, enhanced for
-  //for/in
-  // for(final nome in nomes){
-  //   print(nome);
-  // }
-  // for(int i = 0; i < nomes.length; i++){
-  //   print(nomes[i]);
-  // }
-  // print(nomes[0]);
-  // print(nomes[1]);
-  // print(nomes[3]);
-  // print(nomes.toString());
-  // print(nomes.runtimeType);
+  /*for (int i = 0; i < nomes3.length; i++){
+    print(nomes3.elementAt(i));
+  }*/
+
+  //for each
+  for (final nome in nomes3){
+    print(nome);
+  }
 }
