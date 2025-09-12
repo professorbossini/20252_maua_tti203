5a31be306718f46ae31e568dedc5c9c4599a86ce
Rodrigo Bossini <professorbossini@gmail.com>
2025-09-12 10:59:41 -0300

    ap01-flutter: pg 38
    - vimos sobre a pexels
    - vimos sobre onde armazenar chave de api
    - ver um desenho back/front/api
    - ver json
    - prosseguir com http

--- projetos_flutter/exibe_imagens/lib/main.dart
+++ projetos_flutter/exibe_imagens/lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'src/app.dart';
 void main(){
-  int a = 1;
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Minhas imagens'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          a++;
-          print('Hello');
-        },
-        child: Icon(Icons.camera_alt),
-      ),
-    )
-  );
+  final app = App();
   runApp(app);  
 }
---
+++ projetos_flutter/exibe_imagens/lib/src/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppState extends State <App> {
+  int numeroImagens = 0;
+  String chaveAPI = "a91Qyfh2Ud1rdeOGKV8aTR5Aj9UmRvdma6EdyhC9EfKStoAyt7rmDuhV";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Minhas imagens')),
+        body: Text('Número de imagens: $numeroImagens.'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+          //numeroImagens = numeroImagens + 1;
+          setState(() => numeroImagens = numeroImagens + 1);
+           print(numeroImagens);
+          },
+          child: Icon(Icons.camera_alt),
+        ),
+      ),
+    );
+  }
+}
+
+class App extends StatefulWidget{
+  State <App> createState(){
+    return AppState();
+  }
+}
