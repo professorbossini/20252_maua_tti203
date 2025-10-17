060d2ec9e1a06b7ff3c96c8febee59ac079539c1
evandro <ferraz.evandro@outlook.com>
2025-10-17 11:04:41 -0300

    ap02-flutter: paramos na página 34

--- projetos_flutter/gerenciamento_de_estado/lib/src/blocs/bloc.dart
+++ projetos_flutter/gerenciamento_de_estado/lib/src/blocs/bloc.dart
@@ -7,21 +7,15 @@ class Bloc with Validator{
   final _emailController = StreamController <String> ();
   final _passwordController = StreamController <String> ();
 
-  // getters do objeto Stream
-  // email e password são objetos (da classe Stream) que representam 
-  // a rotina como um todo
+  // email e password são objetos (da classe Stream) que consideram os dados de email e senha já validados
   Stream<String> get email => _emailController.stream.transform(validateEmail);
   Stream<String> get password => _passwordController.stream.transform(validatePassword);
 
-  // validateEmail(campo.text)
-  // retorna efetivamente os valores de texto guardados (do campo de senha e de email)
-  // guardados no bloc (estado centralizado)
-  // sink guarda o valor atual dos campos de texto que serão validados
-  // add, que é o que estamos retornando no get, é uma função que retorna 
-  // o valor atual do campo de texto
+  // sink guarda o valor de email/senha no estado centralizado (bloc)
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
+  // desaloca a memória utilizada para os controllers
   void dispose(){
     _emailController.close();
     _passwordController.close();
--- projetos_flutter/gerenciamento_de_estado/lib/src/blocs/validators.dart
+++ projetos_flutter/gerenciamento_de_estado/lib/src/blocs/validators.dart
@@ -1,33 +1,32 @@
-// mixin = conjunto de instruções de código (funções)
+// mixin = conjunto de instruções de código (funções nesse caso)
 // que podemos depois "injetar" em uma classe
 
 import 'package:email_validator/email_validator.dart';
 import 'dart:async';
 
-mixin Validator{
-
+mixin Validator {
+  // validação do email
   final validateEmail = StreamTransformer<String, String>.fromHandlers(
-    handleData: (email, sink){ // validação do email
-      //transformer representa a formatação atribuida pela função validate()
-      if(EmailValidator.validate(email)){  
-          // se esta tudo certo com o email, adicionamos ao fluxo stream
-          sink.add(email);
-      }else{ //caso contrario, adicionamos um erro
-          sink.addError("E-mail inválido");
+    handleData: (email, sink) {
+      if (EmailValidator.validate(email)) {
+        // se esta tudo certo com o email, adicionamos ao fluxo stream
+        sink.add(email);
+      } else {
+        //caso contrario, adicionamos um erro
+        sink.addError("E-mail inválido");
       }
-    }
+    },
   );
 
   // validação para o campo de senha
-  // validatePassword()
   final validatePassword = StreamTransformer<String, String>.fromHandlers(
-    handleData: (senha, sink){
-      if(senha.length > 3){ //única validação: senha tem que ter no mínimo 4 caracteres
+    handleData: (senha, sink) {
+      if (senha.length > 3) {
+        //única validação: senha tem que ter no mínimo 4 caracteres
         sink.add(senha);
-      }else{ // se a senha não tiver pelo menos 4 caracteres
+      } else {
         sink.addError("Senha deve ter, pelo menos, 4 caracteres");
       }
-    }
+    },
   );
-
 }
