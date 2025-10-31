bdd95f6db9d6b8ba6ed7f7cfaeb6cbe307a4b497
Rodrigo Bossini <professorbossini@gmail.com>
2025-10-31 11:13:03 -0300

    ap02-flutter: encerra a apostila

--- projetos_flutter/gerenciamento_de_estado/lib/src/blocs/bloc.dart
+++ projetos_flutter/gerenciamento_de_estado/lib/src/blocs/bloc.dart
@@ -1,18 +1,30 @@
 import 'dart:async';
 
 import 'package:gerenciamento_de_estado/src/blocs/validators.dart';
-
+import 'package:rxdart/rxdart.dart';
 class Bloc with Validators{
-  final _emailController = StreamController <String>();
-  final _passwordController = StreamController <String>();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   Stream <String> get email => _emailController.stream.transform(validateEmail);
 
   Stream <String> get password => _passwordController.stream.transform(validatePassword);
 
+  Stream <bool> get emailAndPasswordAreOkay => CombineLatestStream.combine2(
+    email, 
+    password, 
+    (e, p) => true
+  );
+
   Function(String) get changeEmail => _emailController.sink.add;
 
   Function(String) get changePassword => _passwordController.sink.add;
+
+  void submitForm(){
+    final email = _emailController.value;
+    final password = _passwordController.value;
+    print('email: $email, senha: $password');
+  }
 
   void dispose(){
     _emailController.close();
--- projetos_flutter/gerenciamento_de_estado/lib/src/blocs/provider.dart
+++ projetos_flutter/gerenciamento_de_estado/lib/src/blocs/provider.dart
@@ -12,6 +12,7 @@ class Provider extends InheritedWidget{
   Provider({Key? key, required Widget child}): super(key: key, child: child);
 
   static Bloc of (BuildContext context){
+    // sound null safety
     return context.dependOnInheritedWidgetOfExactType<Provider>()!.bloc;
   }
   
